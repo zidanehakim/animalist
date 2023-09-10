4acd1056bd7397322b9b858302c946ce39ec568d
unknown <zidanehakimgt@gmail.com>
2023-09-11 00:01:19 +0800

    Membuat throw catch pada script javascript

--- page/script.js
+++ page/script.js
@@ -1,16 +1,15 @@
 let end;
 
 window.addEventListener("scroll", (e) => {
+  console.log(end);
   const totop = document.querySelector("#toTop");
   const tobottom = document.querySelector("#toBottom");
-  console.log(window.scrollY);
   if (window.scrollY > 0) {
     totop.style.display = "block";
   } else {
     totop.style.display = "none";
   }
 
-  console.log(window.scrollY, end);
   if (window.scrollY <= end - 1000) {
     tobottom.style.display = "block";
   } else {
@@ -20,53 +19,78 @@ window.addEventListener("scroll", (e) => {
 
 const submitbtn = document.getElementById("submitbtn");
 submitbtn.addEventListener("click", async (e) => {
+  end = document.querySelector("#end").getBoundingClientRect().top + window.scrollY;
   e.preventDefault();
-  const text = submitbtn.previousElementSibling;
+  let text = submitbtn.previousElementSibling.value;
+  text = text.replace(" ", "%20");
 
-  await fetch("https://api.jikan.moe/v4/anime?q=" + text.value)
+  await fetch("https://api.jikan.moe/v4/anime?q=" + text)
     .then((response) => response.json())
     .then((response) => {
       setAnime(response.data);
+    })
+    .catch((e) => {
+      console.log(e);
+      end = document.querySelector("#end").getBoundingClientRect().top + window.scrollY;
     });
 
-  await fetch("https://api.jikan.moe/v4/manga?q=" + text.value)
+  await fetch("https://api.jikan.moe/v4/manga?q=" + text)
     .then((response) => response.json())
     .then((response) => {
       setManga(response.data);
+    })
+    .catch((e) => {
+      console.log(e);
+      end = document.querySelector("#end").getBoundingClientRect().top + window.scrollY;
     });
 
-  await fetch("https://api.jikan.moe/v4/characters?q=" + text.value)
+  await fetch("https://api.jikan.moe/v4/characters?q=" + text)
     .then((response) => response.json())
     .then((response) => {
       setChar(response.data);
+    })
+    .catch((e) => {
+      console.log(e);
+      end = document.querySelector("#end").getBoundingClientRect().top + window.scrollY;
     });
-
-  end = document.querySelector("#end").getBoundingClientRect().top + window.scrollY;
 });
 
 const form = document.querySelector("form");
 form.addEventListener("submit", async (e) => {
+  end = document.querySelector("#end").getBoundingClientRect().top + window.scrollY;
   e.preventDefault();
-  const text = submitbtn.previousElementSibling;
+  let text = submitbtn.previousElementSibling.value;
+  text = text.replace(" ", "%20");
 
-  await fetch("https://api.jikan.moe/v4/anime?q=" + text.value)
+  await fetch("https://api.jikan.moe/v4/anime?q=" + text)
     .then((response) => response.json())
     .then((response) => {
       setAnime(response.data);
+    })
+    .catch((e) => {
+      console.log(e);
+      end = document.querySelector("#end").getBoundingClientRect().top + window.scrollY;
     });
 
-  await fetch("https://api.jikan.moe/v4/manga?q=" + text.value)
+  await fetch("https://api.jikan.moe/v4/manga?q=" + text)
     .then((response) => response.json())
     .then((response) => {
       setManga(response.data);
+    })
+    .catch((e) => {
+      console.log(e);
+      end = document.querySelector("#end").getBoundingClientRect().top + window.scrollY;
     });
 
-  await fetch("https://api.jikan.moe/v4/characters?q=" + text.value)
+  await fetch("https://api.jikan.moe/v4/characters?q=" + text)
     .then((response) => response.json())
     .then((response) => {
       setChar(response.data);
+    })
+    .catch((e) => {
+      console.log(e);
+      end = document.querySelector("#end").getBoundingClientRect().top + window.scrollY;
     });
-  end = document.querySelector("#end").getBoundingClientRect().top + window.scrollY;
 });
 
 function setAnime(animes) {
@@ -81,15 +105,15 @@ function setAnime(animes) {
 
   for (anime of animes) {
     section1.innerHTML += `<div class="bg-slate-950 rounded overflow-hidden mt-5 m-auto shadow-xl overflow-y-auto hover:scale-105 transition cursor-pointer" style="height:29em;width:16em">
-          <div alt="" style="height:22em;width:100%;background-image: url(${anime.images.jpg.image_url});" class="bg-slate-950 bg-no-repeat m-auto bg-contain
-           bg-center"></div>
-          <div class="m-auto font-sans text-sm text-white">
-            <h2 class="mt-2 mx-4 text-md font-semibold">${anime.title}</h2>
-            <p class="mt-2 mx-4 inline">${anime.year == null ? "Not Found" : anime.year}</p>
-            <p class="mt-2 mx-4 font-semibold">Episodes: <span class="font-normal">${anime.episodes}</span></p>
-            <p class="mt-2 mx-4 font-semibold">Rating: <span class="font-normal">${anime.score}/10⭐</span></p>
-          </div>
-        </div>`;
+            <div alt="" style="height:22em;width:100%;background-image: url(${anime.images.jpg.image_url});" class="bg-slate-950 bg-no-repeat m-auto bg-contain
+             bg-center"></div>
+            <div class="m-auto font-sans text-sm text-white">
+              <h2 class="mt-2 mx-4 text-md font-semibold">${anime.title}</h2>
+              <p class="mt-2 mx-4 inline">${anime.year == null ? "Not Found" : anime.year}</p>
+              <p class="mt-2 mx-4 font-semibold">Episodes: <span class="font-normal">${anime.episodes}</span></p>
+              <p class="mt-2 mx-4 font-semibold">Rating: <span class="font-normal">${anime.score}/10⭐</span></p>
+            </div>
+          </div>`;
   }
 }
 
