2e2e5964409ad5cf00c736428c1d48c0551ea3dd
unknown <zidanehakimgt@gmail.com>
2023-09-17 13:49:13 +0800

    Menambahkan fitur 18 years old dan beberapa adjustments

--- page/script.js
+++ page/script.js
@@ -1,3 +1,5 @@
+const age = confirm("Are you 18 years old or older?");
+
 let end;
 
 window.addEventListener("scroll", (e) => {
@@ -104,16 +106,30 @@ function setAnime(animes) {
   section1.innerHTML = ``;
 
   for (anime of animes) {
-    section1.innerHTML += `<div class="bg-slate-950 rounded overflow-hidden mt-5 m-auto shadow-xl overflow-y-auto hover:scale-105 transition cursor-pointer" style="height:29em;width:16em">
-            <div alt="" style="height:22em;width:100%;background-image: url(${anime.images.jpg.image_url});" class="bg-slate-950 bg-no-repeat m-auto bg-contain
-             bg-center"></div>
-            <div class="m-auto font-sans text-sm text-white">
-              <h2 class="mt-2 mx-4 text-md font-semibold">${anime.title}</h2>
-              <p class="mt-2 mx-4 inline">${anime.year == null ? "Not Found" : anime.year}</p>
-              <p class="mt-2 mx-4 font-semibold">Episodes: <span class="font-normal">${anime.episodes}</span></p>
-              <p class="mt-2 mx-4 font-semibold">Rating: <span class="font-normal">${anime.score}/10⭐</span></p>
-            </div>
-          </div>`;
+    if (age) {
+      section1.innerHTML += `<div class="mx-10 bg-slate-950 rounded overflow-hidden mt-5 m-auto shadow-xl overflow-y-auto hover:scale-105 transition cursor-pointer obj" style="height:29em;width:16em">
+              <div alt="" style="height:22em;width:100%;background-image: url(${anime.images.jpg.image_url});" class="bg-slate-950 bg-no-repeat m-auto bg-contain
+               bg-center"></div>
+              <div class="m-auto font-sans text-sm text-white">
+                <h2 class="mt-2 mx-4 text-md font-semibold">${anime.title}</h2>
+                <p class="mt-2 mx-4 inline">${anime.year == null ? "Not Found" : anime.year}</p>
+                <p class="mt-2 mx-4 font-semibold">Episodes: <span class="font-normal">${anime.episodes}</span></p>
+                <p class="mt-2 mx-4 font-semibold">Rating: <span class="font-normal">${anime.score}/10⭐</span></p>
+              </div>
+            </div>`;
+    } else {
+      rating = anime.rating[0];
+      section1.innerHTML += `<div class="mx-10 bg-slate-950 rounded overflow-hidden mt-5 m-auto shadow-xl overflow-y-auto hover:scale-105 transition cursor-pointer obj" style="height:29em;width:16em">
+              <div alt="" style="height:22em;width:100%;background-image: url(${rating != "R" ? anime.images.jpg.image_url : "../img/restricted.png"});" class="bg-slate-950 bg-no-repeat m-auto bg-contain
+               bg-center"></div>
+              <div class="m-auto font-sans text-sm text-white">
+                <h2 class="mt-2 mx-4 text-md font-semibold">${anime.title}</h2>
+                <p class="mt-2 mx-4 inline">${anime.year == null ? "Not Found" : anime.year}</p>
+                <p class="mt-2 mx-4 font-semibold">Episodes: <span class="font-normal">${anime.episodes}</span></p>
+                <p class="mt-2 mx-4 font-semibold">Rating: <span class="font-normal">${anime.score}/10⭐</span></p>
+              </div>
+            </div>`;
+    }
   }
 }
 
@@ -128,15 +144,28 @@ function setManga(mangas) {
   section2.innerHTML = ``;
 
   for (manga of mangas) {
-    section2.innerHTML += `<div class="bg-slate-950 rounded overflow-hidden mt-5 m-auto shadow-xl overflow-y-auto hover:scale-105 transition cursor-pointer" style="height:29em;width:16em">
-          <div alt="" style="height:22em;width:100%;background-image: url(${manga.images.jpg.image_url});" class=" bg-slate-950 bg-no-repeat m-auto bg-contain bg-center"></div>
-          <div class="m-auto font-sans text-sm text-white">
-            <h2 class="mt-2 mx-4 text-md font-semibold">${manga.title}</h2>
-            <p class="mt-2 mx-4 inline">${manga.authors[0].name}</p>
-            <p class="mt-2 mx-4 font-semibold">Chapters: <span class="font-normal">${manga.chapters}</span></p>
-            <p class="mt-2 mx-4 font-semibold">Rating: <span class="font-normal">${manga.score}/10⭐</span></p>
-          </div>
-        </div>`;
+    if (age) {
+      section2.innerHTML += `<div class="bg-slate-950 rounded overflow-hidden mt-5 m-auto shadow-xl overflow-y-auto hover:scale-105 transition cursor-pointer obj" style="height:29em;width:16em">
+            <div alt="" style="height:22em;width:100%;background-image: url(${manga.images.jpg.image_url});" class=" bg-slate-950 bg-no-repeat m-auto bg-contain bg-center"></div>
+            <div class="m-auto font-sans text-sm text-white">
+              <h2 class="mt-2 mx-4 text-md font-semibold">${manga.title}</h2>
+              <p class="mt-2 mx-4 inline">${manga.authors[0].name}</p>
+              <p class="mt-2 mx-4 font-semibold">Chapters: <span class="font-normal">${manga.chapters}</span></p>
+              <p class="mt-2 mx-4 font-semibold">Rating: <span class="font-normal">${manga.score}/10⭐</span></p>
+            </div>
+          </div>`;
+    } else {
+      rating = manga.rating[0];
+      section2.innerHTML += `<div class="bg-slate-950 rounded overflow-hidden mt-5 m-auto shadow-xl overflow-y-auto hover:scale-105 transition cursor-pointer obj" style="height:29em;width:16em">
+            <div alt="" style="height:22em;width:100%;background-image: url(${rating != "r" ? manga.images.jpg.image_url : "../img/restricted.png"});" class=" bg-slate-950 bg-no-repeat m-auto bg-contain bg-center"></div>
+            <div class="m-auto font-sans text-sm text-white">
+              <h2 class="mt-2 mx-4 text-md font-semibold">${manga.title}</h2>
+              <p class="mt-2 mx-4 inline">${manga.authors[0].name}</p>
+              <p class="mt-2 mx-4 font-semibold">Chapters: <span class="font-normal">${manga.chapters}</span></p>
+              <p class="mt-2 mx-4 font-semibold">Rating: <span class="font-normal">${manga.score}/10⭐</span></p>
+            </div>
+          </div>`;
+    }
   }
 }
 
@@ -151,7 +180,7 @@ function setChar(chars) {
   section3.innerHTML = ``;
 
   for (char of chars) {
-    section3.innerHTML += `<div class="bg-slate-950 rounded overflow-hidden mt-5 m-auto shadow-xl overflow-y-auto hover:scale-105 transition cursor-pointer" style="height:27em;width:12em">
+    section3.innerHTML += `<div class="bg-slate-950 rounded overflow-hidden mt-5 m-auto shadow-xl overflow-y-auto hover:scale-105 transition cursor-pointer obj" style="height:27em;width:12em">
           <div alt="" style="height:20em;width:100%;background-image: url(${char.images.jpg.image_url});" class=" bg-slate-950 bg-no-repeat m-auto bg-contain bg-center"></div>
           <div class="m-auto font-sans text-sm text-white">
             <h2 class="mt-2 mx-4 text-md font-semibold">${char.name}</h2>
